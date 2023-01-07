b1e6372e7eb487cd154da6686706cbc473a309bc
webshell_user <webshell_user@mail.com>
2023-01-07 15:23:21 +0000

    first time push

---
+++ bo.sh
@@ -0,0 +1,26 @@
+# nano CUT(del) line: "CTRL+K"    `Kut`
+# nano paste line: "CTRL+U"       `Unkut`
+# nano UNDO: "Alt+U" / REDO: "Alt+E"
+
+nl="\n"
+SUB="Not Found"
+
+date
+
+if [ "$1" == "" ] || [ "$2" == "" ] ; then
+  printf "You forgot a range"$nl
+  exit 0
+fi
+
+for (( i=$1; i<=$2; i++ )); do                 #for (( c=1; c<=100000; c++ )); do       #for i  in {1..100000}
+   curl_o=$(curl -s http://challenges.cyber.org.il/bo/$i.html &)
+
+   if [[ "$curl_o" == *"$SUB"* ]]; then        # check if sub-string included in string
+     :                                         # do nothing
+   else
+     printf $curl_o$nl$i$nl
+     date
+     printf $nl$nl
+   fi
+done
+date
