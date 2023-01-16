ff8ccd64a8ec9212475f7a28cdb1184754cca244
pi@rpi3b <pi.mail@mail.com>
2023-01-16 10:56:34 +0200

    fix: printf,curl_o,created another bug

--- bo.sh
+++ bo.sh
@@ -5,6 +5,7 @@
 nl="\n"
 SUB="Not Found"
 
+printf "************ START ******************************"
 date
 
 if [ "$1" == "" ] || [ "$2" == "" ] ; then
@@ -13,14 +14,17 @@ if [ "$1" == "" ] || [ "$2" == "" ] ; then
 fi
 
 for (( i=$1; i<=$2; i++ )); do                 #for (( c=1; c<=100000; c++ )); do       #for i  in {1..100000}
-   curl_o=$(curl -s http://challenges.cyber.org.il/bo/$i.html &)
+   curl_o=$(curl -s http://challenges.cyber.org.il/bo/$i.html)
 
    if [[ "$curl_o" == *"$SUB"* ]]; then        # check if sub-string included in string
      :                                         # do nothing
-   else
-     printf $curl_o$nl$i$nl
+   else 
+     printf "i=""$i""$nl$curl_o$nl"
      date
      printf $nl$nl
    fi
 done
+
 date
+printf "********* END ******************************"
+
